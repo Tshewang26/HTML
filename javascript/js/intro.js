5c861932362c0981b89f2e6d8084da963a15af67
Tshewang26 <05240157.jnec@rub.edu.bt>
2025-09-16 15:08:41 +0600

    add javaScript file

---
+++ javascript/js/intro.js
@@ -0,0 +1,15 @@
+function changeColor() {
+       document.getElementById("div_3").innerHTML="<h1>Pink</h1>";
+}
+
+function replaceEverything() {
+            document.write("Everything is replaced");
+}
+
+function writeIntoTheLog() {
+    console.log("Debugging");
+}
+
+function displayAlert() {
+    window.alert("This is an Alret");
+}
